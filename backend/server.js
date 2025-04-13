e31d6ca5c9bc2fa3115fbc478ecef2aa9f68cfd1
Faran <faran.7j99@gmail.com>
2025-04-13 13:05:06 +0330

    fix: add comments for clarity on dotenv configuration and middleware usage in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,19 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
 // for accessing MONGO_URI
-dotenv.config();
+dotenv.config(); // loads environment variables from a .env file into process.env
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/api/products", router);
+app.use(cors()); // to allow cross-origin requests
+app.use(express.json()); // to parse JSON data from requests
+app.use("/api/products", router); // to handle requests to the products API
 
 app.listen(5000, () => {
   connectDB();
   console.log("Server's at http://localhost:5000");
 });
+
+
+/*
+    Middleware: A function that has access to the request object (req), the response object (res), and the next middleware function in the application’s request-response cycle. It's used to modify the request and response objects, end the request-response cycle, and call the next middleware function in the stack.
+*/
