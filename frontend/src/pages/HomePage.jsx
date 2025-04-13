c88580ed00edb6b27ce36dba54f8ab890e2644ae
Faran <faran.7j99@gmail.com>
2025-04-13 08:38:30 +0330

    feat: add no product found message (front)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,8 +4,12 @@ import { getProducts } from '../services/products';
 import ErrorBlock from '../components/common/ErrorBlock';
 import Spinner from '../components/common/Spinner';
 import ProductList from '../components/products/ProductList';
+import { Link } from 'react-router';
+import useTheme from '../store/theme-mode';
 
 export default function HomePage() {
+    const lightMode = useTheme(state => state.lightMode);
+
     const productsQuery = useQuery({
         queryKey: ['products'],
         queryFn: () => getProducts()
@@ -24,8 +28,19 @@ export default function HomePage() {
                 {
                     productsQuery.isError ?
                         <ErrorBlock error={productsQuery.error} /> : productsQuery.isFetching ?
-                            <Spinner /> :
-                            <ProductList products={products} />
+                            <Spinner /> : products.length ?
+                                <ProductList products={products} /> :
+                                <div
+                                    className={`${lightMode ? "text-black" : "text-white"} flex justify-center gap-1 w-[100%] mt-10`}
+                                >
+                                    <p>No products found.</p>
+                                    <Link
+                                        to={"register-product"}
+                                        className='text-blue-600 font-bold hover:underline'
+                                    >
+                                        Register a product.
+                                    </Link>
+                                </div>
                 }
             </section>
         </>
