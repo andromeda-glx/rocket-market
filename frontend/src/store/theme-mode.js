dc9bdac4181bf69ff475927c0268750c368eb52f
Faran <faran.7j99@gmail.com>
2025-04-13 08:07:20 +0330

    fix: persist theme mode and exclude theme actions (front)

--- frontend/src/store/theme-mode.js
+++ frontend/src/store/theme-mode.js
@@ -1,16 +1,29 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useTheme = create((set) => {
-  return {
-    lightMode: false,
-    actions: {
-      switchMode: () => {
-        set((state) => {
-          return { lightMode: !state.lightMode };
-        });
-      },
+const useTheme = create(
+  persist(
+    (set) => {
+        const switchMode = () => {
+            set(state => ({lightMode: !state.lightMode}));
+        }
+
+      return {
+        lightMode: false,
+        actions: {
+          switchMode,
+        },
+      };
     },
-  };
-});
+    {
+        name: "theme-storage",
+        partialize: state => ({lightMode: state.lightMode})
+    //   partialize: (state) =>
+    //     Object.fromEntries(
+    //       Object.entries(state).filter(([key]) => !["actions"].includes(key))
+    //     ),
+    }
+  )
+);
 
 export default useTheme;
